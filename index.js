3fcdca9808295e13b6120370179308488af11ea3
glorious-number-5611 <rloiseau99@yahoo.com>
2020-08-27 12:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function theBeatlesPlay(array, array1) {
     var array2 = [];
     for (let i = 0; i < array.legnth; i++) {
       console.log(`${array[i]} plays ${array1[i]}`)
-      array2.push();
+      array2[i];
     }
     return array2
   }
-  
