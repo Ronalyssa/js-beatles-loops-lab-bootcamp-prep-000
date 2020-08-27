3aeee952f84172777e64e00ece269daed0101709
glorious-number-5611 <rloiseau99@yahoo.com>
2020-08-27 13:08:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // add solution here
-let i = 0;
-
-  function incrementVariable() {
-    i = i + 1;
-    return i;
-  }
-
-function theBeatlesPlay(array, array1) {
-    var array2 = [];
-    for (let i = 0; i < array.legnth; i++) {
-      console.log(`${array[i]} plays ${array1[i]}`)
-      array2[i];
-    }
-    return array2
-  }
+// let i = 0;
+//
+//   function incrementVariable() {
+//     i = i + 1;
+//     return i;
+//   }
+//
+// function theBeatlesPlay(array, array1) {
+//     var array2 = [];
+//     for (let i = 0; i < array.legnth; i++) {
+//       console.log(`${array[i]} plays ${array1[i]}`)
+//       array2[i];
+//     }
+//     return array2
+//   }
 
   function johnLennonFacts(array) {
     while (i < array.legnth) {
